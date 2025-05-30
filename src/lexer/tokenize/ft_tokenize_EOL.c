aca7b599c15d4c6ac48178e0d5f440d6db901b3e
cuycorp <curcorp@gmail.com>
2025-05-30 16:56:30 +0200

    add tokenize_EOL and fixed bug in tokenize_pipe

--- src/lexer/tokenize/ft_tokenize_EOL.c
+++ src/lexer/tokenize/ft_tokenize_EOL.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tokenize_pipe.c                                     :+:      :+:    :+:   */
+/*   ft_tokenize_EOL.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcamaren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/29 16:56:48 by mcamaren          #+#    #+#             */
-/*   Updated: 2025/05/29 16:56:51 by mcamaren         ###   ########.fr       */
+/*   Created: 2025/05/30 16:44:56 by mcamaren          #+#    #+#             */
+/*   Updated: 2025/05/30 16:44:57 by mcamaren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_tokenize_pipe(char *str, unsigned int *pos, t_shell *data)
+void	ft_tokenize_EOL(char *str, unsigned int *pos, t_shell *data)
 {
 	t_token	*new_token;
 	char	*token;
@@ -20,10 +20,10 @@ void	ft_tokenize_pipe(char *str, unsigned int *pos, t_shell *data)
 
 	length = 1;
 	(*pos) += length;
-	token = ft_strdup("|");
+	token = ft_strdup("\0");
 	if (!token)
 		return (ft_close_program(data, EXIT_FAILURE));
-	new_token = ft_create_token(token, TOKEN_PIPE);
+	new_token = ft_create_token(token, TOKEN_END_OF_LINE);
 	if (!new_token)
 		return (free(token));
 	ft_add_token(&data->tokens_list, new_token);
