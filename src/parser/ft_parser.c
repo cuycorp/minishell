278b56d63e619a428b9f3028b4a1b400a9e42ee4
cuycorp <curcorp@gmail.com>
2025-06-06 16:14:51 +0200

    added ft_parse_word and ft_parse_redirection functions. Updated ft_parse_simple_command with these functions

--- src/parser/ft_parser.c
+++ src/parser/ft_parser.c
@@ -18,9 +18,9 @@ bool	ft_parse_command_line(t_token **token_list, t_shell *data);
 bool	ft_parse_grouped_pipeline(t_token **token_list, t_shell *data);
 bool	ft_parse_pipeline(t_token **token_list, t_shell *data);
 bool	ft_parse_simple_command(t_token **token_list, t_shell *data);
-bool	ft_parse_redirection(t_token *token_list, t_shell *data);
+bool	ft_parse_redirection(t_token **token_list, t_shell *data);
 bool	ft_parse_arguments(t_token *token_list, t_shell *data);
-bool	ft_parse_word(t_token *token_list, t_shell *data);
+bool	ft_parse_word(t_token **token_list, t_shell *data);
 bool	ft_parse_quoted_string(t_token *token_list, t_shell *data);
 bool	ft_parse_single_quoted(t_token *token_list, t_shell *data);
 bool	ft_parse_double_quoted_string(t_token *token_list, t_shell *data);
@@ -28,18 +28,43 @@ bool	ft_parse_double_quoted_char(t_token *token_list, t_shell *data);
 bool	ft_parse_env_variable(t_token *token_list, t_shell *data);
 bool	ft_parse_env_name(t_token *token_list, t_shell *data);
 
-bool	ft_parse_simple_command(t_token **token_list, t_shell *data)
+bool	ft_parse_word(t_token **token_list, t_shell *data)
 {
 	(void)data;
-	ft_printf(1, "in ft_parse_simple_command, curent = %s \n",
+	ft_printf(1, "in ft_parse_word, curent = %s \n",
 		(*token_list)->value);
 	if ((*token_list)->type == TOKEN_WORD)
 	{
 		*token_list = (*token_list)->next;
-		return (ft_printf(1, "ft_parse_simple_command = true\n"), true);
+		return (ft_printf(1, "ft_parse_word = true\n"), true);
+	}
+	return (perror("Error: is not of type TOKEN_WORD\n"), false);
+}
+
+bool	ft_parse_redirection(t_token **token_list, t_shell *data)
+{
+	ft_printf(1, "in ft_parse_redirection, curent = %s \n",
+		(*token_list)->value);
+	if (ft_is_redirection_type((*token_list)->type))
+	{
+		*token_list = (*token_list)->next;
+		ft_parse_word(token_list, data);
+		return (ft_printf(1, "ft_parse_redirection is true\n"), true);
 	}
 	else
-		return (perror("Error: is not of type TOKEN_WORD\n"), false);
+		return (perror("Error: ft_parse_redirection failed"), false);
+}
+
+bool	ft_parse_simple_command(t_token **token_list, t_shell *data)
+{
+	ft_printf(1, "in ft_parse_simple_command, curent = %s \n",
+		(*token_list)->value);
+	while (ft_is_redirection_type((*token_list)->type))
+	{
+		if (!ft_parse_redirection(token_list, data))
+			return (perror("Error: ft_parse_redirection failed"), false);
+	}
+	return (ft_parse_word(token_list, data));
 }
 
 bool	ft_parse_pipeline(t_token **token_list, t_shell *data)
@@ -48,19 +73,16 @@ bool	ft_parse_pipeline(t_token **token_list, t_shell *data)
 		return (perror("Error: ft_parse_simple_command failed\n"), false);
 	while ((*token_list))
 	{
-		ft_printf(1, "current %s \n", (*token_list)->value);
+		ft_printf(1, "in ft_parse_pipeline, current = %s \n", (*token_list)->value);
 		if ((*token_list)->type == TOKEN_PIPE)
 		{
 			*token_list = (*token_list)->next;
 			if (!ft_parse_simple_command(token_list, data))
 				return (perror("Error: ft_parse_simple_command failed\n"),
 					false);
-			*token_list = (*token_list)->next;
 		}
 		else
-		{
 			return (perror("Error: is not type of TOKEN_PIPE"), false);
-		}
 	}
 	return (true);
 }
---
+++ src/utils/ft_utils.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcamaren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/06 16:09:43 by mcamaren          #+#    #+#             */
+/*   Updated: 2025/06/06 16:09:45 by mcamaren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+bool	ft_is_redirection_type(t_token_type type)
+{
+	return (type == TOKEN_REDIRECT_IN || type == TOKEN_REDIRECT_OUT
+		|| type == TOKEN_HEREDOC || type == TOKEN_APPEND_OUT);
+}
