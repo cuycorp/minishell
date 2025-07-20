86d78ed40e5f5cc1fc0bf7ff9e1eef5f0e34b769
cuycorp <cuycorp@gmail.com>
2025-07-20 18:48:02 +0200

    feat(builtin) fixed bugs and clear leaks and segfaults echo, cd and pwd

--- src/builtins/function_echo.c
+++ src/builtins/function_echo.c
@@ -12,22 +12,40 @@
 
 #include "minishell.h"
 
-int	function_echo(t_command command)
+static bool ft_validate_option(char *option, int *i)
+{
+	bool nl_flag;
+
+	nl_flag = true;
+	if (!option)
+		return (nl_flag);
+	if (ft_strncmp("-n", option, ft_strlen(option)) == 0)
+	{
+		nl_flag = false;
+		(*i)++;
+	}
+	return (nl_flag);
+}
+
+
+
+
+int	function_echo(t_command *command)
 {
 	bool	nl_flag;
 	int		i;
 
 	i = 0;
-	nl_flag = false;
-	if (ft_strncmp("-n", command.args[i], ft_strlen("-n")) == 0)
+	if (!command->args[0])
+		return (1);
+	if (ft_validate_command(command, "echo") == false)
+		return (1);
+	i = 1;
+	nl_flag = ft_validate_option(command->args[i], &i);
+	while (command->args[i])
 	{
-		nl_flag = true;
-		i++;
-	}
-	while (command.args[i])
-	{
-		ft_printf(STDOUT_FILENO, "%s", command.args[i]);
-		if (command.args[i + 1])
+		ft_printf(STDOUT_FILENO, "%s", command->args[i]);
+		if (command->args[i + 1])
 			ft_printf(STDOUT_FILENO, " ");
 		i++;
 	}
@@ -35,3 +53,12 @@ int	function_echo(t_command command)
 		ft_printf(STDOUT_FILENO, "\n");
 	return (0);
 }
+
+/*
+check command
+	+ command name
+	+ command first argument
+check option
+	if correct set flag to put new line and jump to next
+loop over arguments printing
+*/
