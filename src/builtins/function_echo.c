fd0b9fc9bed2dc33d82f17691374fbb967d8fabb
cuycorp <curcorp@gmail.com>
2025-07-23 18:51:28 +0200

    fixed edge case echo -nnn

--- src/builtins/function_echo.c
+++ src/builtins/function_echo.c
@@ -12,23 +12,61 @@
 
 #include "minishell.h"
 
-static bool ft_validate_option(char *option, int *i)
+static bool	ft_validate_option(char *option, int *i)
 {
-	bool nl_flag;
+	bool	nl_flag;
+	int		j;
 
+	j = 0;
 	nl_flag = true;
 	if (!option)
 		return (nl_flag);
-	if (ft_strncmp("-n", option, ft_strlen(option)) == 0)
+	if (option[j] != '-')
+		return (nl_flag);
+	j++;
+	while (option[j])
 	{
-		nl_flag = false;
-		(*i)++;
+		if (option[j] == 'n')
+			nl_flag = false;
+		else
+		{
+			nl_flag = true;
+			break ;
+		}
+		j++;
 	}
+	if (nl_flag == false)
+		(*i)++;
 	return (nl_flag);
 }
 
+static bool	ft_is_valid_skip(char *arg)
+{
+	int	i;
 
+	i = 0;
+	if (arg[i] != '-')
+		return (false);
+	i++;
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (true);
+}
 
+static void	ft_skip_option(char **args, int *i)
+{
+	while (args[*i])
+	{
+		if (ft_is_valid_skip(args[*i]))
+			(*i)++;
+		else
+			break ;
+	}
+}
 
 int	function_echo(t_command *command)
 {
@@ -42,6 +80,7 @@ int	function_echo(t_command *command)
 		return (1);
 	i = 1;
 	nl_flag = ft_validate_option(command->args[i], &i);
+	ft_skip_option(command->args, &i);
 	while (command->args[i])
 	{
 		ft_printf(STDOUT_FILENO, "%s", command->args[i]);
