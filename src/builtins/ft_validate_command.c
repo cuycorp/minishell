15199ef75ffcf7fa3ef68f5500dadc67a4ae5389
cuycorp <curcorp@gmail.com>
2025-08-08 13:10:03 +0200

    feat (builtints) fixed error message for wrong name for command

--- src/builtins/ft_validate_command.c
+++ src/builtins/ft_validate_command.c
@@ -14,14 +14,8 @@
 
 bool	ft_validate_command(t_command *command, char *commmand_name)
 {
-	if (ft_strncmp(commmand_name, command->args[0],
-			ft_strlen(command->args[0])))
-		return (false);
-	if (ft_strncmp(commmand_name, command->args[0], ft_strlen(commmand_name)))
-		return (false);
-	if (ft_strncmp(commmand_name, command->name, ft_strlen(command->name)))
-		return (false);
-	if (ft_strncmp(commmand_name, command->name, ft_strlen(commmand_name)))
-		return (false);
+	if (ft_strncmp(commmand_name, command->name, ft_strlen(commmand_name) + 1))
+		return (ft_printf(STDERR_FILENO, "%s: command not found\n",
+				command->name), false);
 	return (true);
 }
